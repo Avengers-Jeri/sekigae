df127fb050e8de9e74cd7c03775bbcdc58a22d95
saburokirameki <saburoukirameki@kirameki.com>
2018-06-27 05:47:21 +0000

    aaa

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,7 +5,7 @@
         <aside class="col-xs-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title">{{ $user->team_name }}</h3>
+                    <h3><class="panel-title">{{ $user->team_name }}</h3>
                 </div>
                 @if (count($members) > 0)
                 @include('members.members', ['members' => $members])
